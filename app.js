b1e19177f7ec7c87b1d10c43447cc313ac61195c
albarkip <berryprad15@gmail.com>
2020-03-14 19:17:20 +0700

    boitp2

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const DB_USER = [
     {
-        username: "barbar",
-        password: "password"
+        username: "",
+        password: ""
     }
 ]
 
 const inputUser = document.querySelector("#username");
 const inputPass = document.querySelector("#password");
+const inputLagi = document.querySelector("#passwordLagi")
 const buttonSubmit = document.querySelector("#submitForm");
 
 buttonSubmit.addEventListener('click', (e) => {
     e.preventDefault();
     const valueInputPass = inputPass.value;
     const valueInputUser = inputUser.value;
+    const valueInputLagi = inputLagi.value;
+    
     console.log(valueInputPass);
     console.log(valueInputUser);
-
+    console.log(valueInputLagi);
+    DB_USER.map((data) => {
+        data.username = valueInputUser;
+        data.password = valueInputPass;
+    })
     let flag = 0;
     
     DB_USER.map((data) => {
-        if(data.username === valueInputUser && data.password === valueInputPass){
+        if(data.password === valueInputLagi){
             flag = 1;
         }
     })
@@ -30,6 +37,6 @@ buttonSubmit.addEventListener('click', (e) => {
         warningMsg.className = "text-danger";
     }
     else{
-        alert("selamat anda masuk home");
+        alert("selamat anda berhasil registrasi");
     }
 })
